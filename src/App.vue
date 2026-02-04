96bd90874d3b378e85f85277a971524e84f5b38d
Rizki Mufid <rizki.mufid@skyworx.co.id>
2026-02-05 00:05:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Slider } from '@ark-ui/vue/slider'
+</script>
+
+<template>
+  <Slider.Root :default-value="[40]">
+    <div :style="{ display: 'flex', justifyContent: 'space-between' }">
+      <Slider.Label>Label</Slider.Label>
+      <Slider.ValueText />
+    </div>
+    <Slider.Control>
+      <Slider.Track>
+        <Slider.Range />
+      </Slider.Track>
+      <Slider.Thumb :index="0">
+        <Slider.HiddenInput />
+      </Slider.Thumb>
+    </Slider.Control>
+  </Slider.Root>
+</template>
